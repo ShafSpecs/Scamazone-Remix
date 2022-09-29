e35ca81dc1d35b5210ff8cd333b33b368877c238
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2022-09-29 23:17:52 +0100

    Carousel for bigger screens index

---
+++ app/components/home/CarouselComponent.tsx
@@ -0,0 +1,30 @@
+import { Carousel } from "flowbite-react";
+
+export default function CustomCarousel() {
+  return (
+    <div className="h-56 sm:h-64 xl:h-80 2xl:h-96 max-w-7xl mt-8">
+      <Carousel slideInterval={7500}>
+        <img
+          src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
+          alt="..."
+        />
+        <img
+          src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
+          alt="..."
+        />
+        <img
+          src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
+          alt="..."
+        />
+        <img
+          src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
+          alt="..."
+        />
+        <img
+          src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
+          alt="..."
+        />
+      </Carousel>
+    </div>
+  )
+}
